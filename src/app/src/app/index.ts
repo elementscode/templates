3522b59b00c73669d1e62cc83b2a217590034401
Chris Mather <chris@elements.dev>
2021-03-15 13:21:39 -0700

    revert rendering engine code in app template on master for now

--- src/app/src/app/index.ts
+++ src/app/src/app/index.ts
@@ -1,12 +1,8 @@
 import { Application, IRequest } from '@elements/application';
-import ReactRenderEngine from '@elements/application/render/react';
 
 // create the elements application.
 let app = new Application();
 export default app;
-
-// render react pages.
-app.addRenderEngine(new ReactRenderEngine());
 
 // server only code.
 app.server(require('./server'));
